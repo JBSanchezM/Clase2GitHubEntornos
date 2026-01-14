87760a4759f40184b29d0f3d5e19358075f67acc
jhon143jm@gmail.com <jsm0092@alu.medac.es>
2026-01-14 14:06:56 +0100

    Ejercicio 3

--- PooClase1/src/App.java
+++ PooClase1/src/App.java
@@ -1,32 +1,31 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+
         Coche c1 = new Coche();
         c1.color = "Azul";
         c1.anyo = 2007;
         c1.marca = "Audi";
         c1.modelo = "A9";
-        
+
         Coche toyota = new Coche();
         toyota.color = "Gris";
         toyota.anyo = 2007;
         toyota.marca = "Audi";
         toyota.modelo = "A8";
 
-        System.out.println("El año del toyota es: "+toyota.anyo);
+        System.out.println("El año del toyota es: " + toyota.anyo);
         toyota.anyo = 2015;
-        System.out.println("El año del toyota es: "+toyota.anyo);
+        System.out.println("El año del toyota es: " + toyota.anyo);
 
         Coche bmw = new Coche("Rojo", 2000, "BMW", "MS", 100.0, 4);
         Coche mercedes = new Coche(2000, "Mercedes", "Clase E", 50000);
-        System.out.println("El coche "+ toyota.marca +" de "+ toyota.modelo +" con "+ toyota.kilometraje +" KM" + " es del año "+ toyota.anyo);   
-
+        System.out.println("El coche " + toyota.marca + " de " + toyota.modelo + " con " + toyota.kilometraje + " KM"
+                + " es del año " + toyota.anyo);
 
         Profesor pablo = new Profesor("z845d", "pablo Gonzalez", 1300);
         Asignatura sistemas = new Asignatura("Sistemas Informaticos", pablo);
-        System.out.println("Llamando el metodo sistemas: "+ pablo.dni);
-        System.out.println("El salario del profe de sistemas es: "+ sistemas.docente.salario);
-
+        System.out.println("Llamando el metodo sistemas: " + pablo.dni);
+        System.out.println("El salario del profe de sistemas es: " + sistemas.docente.salario);
 
         CuentaBancaria cuenta1 = new CuentaBancaria();
         System.out.println("Saldo inicial de la cuenta 1 es: " + cuenta1.balance);
@@ -41,14 +40,10 @@ public class App {
 
         calc.binevenida("Cuau");
         calc.binevenida("Marina");
-        
-        
-      
 
+        int resultado2 = calc.elevado(5);
+        System.out.println("El resultado es: " + resultado2);
 
     }
-
-
-   
 
 }
--- PooClase1/src/Calculadora.java
+++ PooClase1/src/Calculadora.java
@@ -1,18 +1,17 @@
 public class Calculadora {
-    
-public int restar(int n1, int n2){
 
-    return n1 - n2;
-}
+    public int restar(int n1, int n2) {
 
+        return n1 - n2;
+    }
 
+    public void binevenida(String usuario) {
+        System.out.println("Hola " + usuario);
 
-public void binevenida (String usuario){
-System.out.println("Hola " + usuario);
+    }
 
-}
-
-
-
+    public int elevado(int n1) {
+        return n1 * n1;
+    }
 
 }
